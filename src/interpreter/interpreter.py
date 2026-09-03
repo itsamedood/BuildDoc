097026dc603d1b4f470f11a7a7db854e56389924
dood <itsamedood@gmail.com>
2026-09-03 02:50:36 -0400

    In theory the lexer works, too tired to test, will do tomorrow, goodnight (even at 2:50 am).

--- src/interpreter/interpreter.py
+++ src/interpreter/interpreter.py
@@ -1,9 +1,20 @@
+from interpreter.lexer import Lexer
 from pathlib import Path
 
 
 class Interpreter:
-  def __init__(self, path: Path) -> None:
-    with open(path, "rb") as script:
-      self.raw = script.read()
+  """
+  Handles invoking the lexer and parser.
+  """
 
+  lexer = Lexer()
+
+  def __init__(self, path: Path) -> None:
+    with open(path, "r") as script:
+      self.code = script.read()
+
+    # Lexer!
+    self.lexer.tokenize(self.code)
+
+    # Parser!
     ...
--- src/interpreter/lexer.py
+++ src/interpreter/lexer.py
@@ -1,6 +1,5 @@
 from interpreter.token import Token
-from pathlib import Path
-from typing import Iterator
+from out import BuildDocError
 
 
 class Lexer:
@@ -8,8 +7,36 @@ class Lexer:
   Tokenizes the script into tokens for the parser. Ignores comments.
   """
 
+  tokens: list[tuple[Token, str]]
+  comment = False
+
   def __init__(self) -> None: ...
 
-  # Tokenizes code into an iterator to yield later for O(1) time.
-  def tokenize(self, code: bytes) -> Iterator[Token]:
-    ...
+  def tokenize(self, code: str) -> None: # Iterator[Token]:
+    """
+    Tokenizes code into tokens (duh) for the parser.
+
+    Ignores comments entirely.
+    """
+
+    for char in code:
+      try:
+        # Probably gonna be more important later on.
+        if char == Token.NEWLINE.value:
+          self.comment = False
+          ...  # 2 steps ahead?
+
+        if char == Token.POUND.value:
+          self.comment = True
+          continue
+
+        if self.comment: continue
+
+        self.tokens.append(
+          (Token.LETTER, char) if char in Token.LETTER.value else
+          (Token.NUMBER, char) if char in Token.NUMBER.value else
+          (Token(char), char)
+        )
+      except ValueError:
+        raise BuildDocError("Unknown character: '%s'." %char, 1)
+        # self.tokens.append((Token.ANY, char))
