a4fd16ca355c0e4b745e552d54ad5f298fd49f81
itsamedood <itsamedood@gmail.com>
2026-09-17 20:06:04 -0400

    Working on the parser, just some groundwork right now."

--- src/interpreter/parser.py
+++ src/interpreter/parser.py
@@ -1,5 +1,15 @@
+from enum import Enum
 from interpreter.token import Token  # Sleepy?
 from typing import Any
+
+
+class ReadingType:
+  NONE                = 0  # Or maybe make this default to reading a task command?
+  VARIABLE_NAME       = 1
+  VARIABLE_VALUE      = 2
+  TASK_NAME           = 3
+  TASK_CMD            = 4
+  SHELL_CMD           = 5
 
 
 class Parser:
@@ -7,6 +17,7 @@ class Parser:
 
   line, character = 0, 0
   variables: list[tuple[str, Any]]
+  reading = 0
 
   def __init__(self) -> None: ...
 
